de03b516214fcd5dcf61f6689c0e0b9a10cfd213
Tanaydin Sirin <tanaydin.sirin@finqle.nl>
2022-12-08 13:09:33 +0100

    Better checking for which numbers to candidate as factor.

--- jumper.c
+++ jumper.c
@@ -5,21 +5,23 @@
 #include <time.h>
 
 // Initialize variables.
-mpz_t zero, one, two, seven, eight, exponent, exponent2, bigNumber, start, check, mod;
+mpz_t zero, one, two, seven, eight, exponent, exponent2, exponent8, bigNumber, start, checkMod1, checkMod7, mod;
 int ctrl_c_count = 0;
 time_t last_ctrl_c_time = 0;
 
 static void sig_handler(int _)
 {
     ctrl_c_count++;
-    gmp_printf("\nCurrent : %Zd\n", start);
-    FILE *fptr;
-    char buffer[32]; // The filename buffer.
-    snprintf(buffer, sizeof(char) * 32, "%s.res", mpz_get_str(buffer, 10, exponent));
-    fptr = fopen(buffer, "w");
-    gmp_fprintf(fptr, "%Zd", start);
-    fclose(fptr);
-    if (ctrl_c_count >= 2 && time(NULL) - last_ctrl_c_time <= 5)
+    gmp_printf("\nCheckMod1  : %Zd\n", checkMod1);
+    gmp_printf("CheckMod7  : %Zd\n", checkMod7);
+    // Gonna use this for resuming, commenting out for now.
+    // FILE *fptr;
+    // char buffer[32]; // The filename buffer.
+    // snprintf(buffer, sizeof(char) * 32, "%s.res", mpz_get_str(buffer, 10, exponent));
+    // fptr = fopen(buffer, "w");
+    // gmp_fprintf(fptr, "%Zd", start);
+    // fclose(fptr);
+    if (ctrl_c_count >= 2 && time(NULL) - last_ctrl_c_time <= 2)
     {
         printf("Exiting program...\n");
         exit(0);
@@ -30,11 +32,31 @@ static void sig_handler(int _)
     }
 }
 
+void check_factor(mpz_t number_to_check, mpz_t factor_to_check)
+{
+    // Checking if number is probably prime
+    switch (mpz_probab_prime_p(factor_to_check, 50))
+    {
+    case 1:
+    case 2:
+        mpz_mod(mod, number_to_check, factor_to_check);
+        if (mpz_cmp(mod, zero) == 0)
+        {
+            gmp_printf("\nFactor  : %Zd\n", factor_to_check);
+            exit(0);
+        }
+        break;
+    case 0:
+    default:
+        break;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     signal(SIGINT, sig_handler);
 
-    mpz_inits(zero, one, two, seven, eight, exponent, exponent2, bigNumber, start, check, mod, (mpz_ptr)0);
+    mpz_inits(zero, one, two, seven, eight, exponent, exponent2, exponent8, bigNumber, start, checkMod1, checkMod7, mod, (mpz_ptr)0);
     mpz_set_ui(one, 1);
     mpz_set_ui(two, 2);
     mpz_set_ui(seven, 7);
@@ -54,15 +76,20 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
+    // Calculate exponent * 2 for using it while searching for a good number to start (1 in mod exponent).
     mpz_mul(exponent2, exponent, two);
+
+    // Calculate exponent * 8 for keeping factoring numbers 1 or 7 in mod 8
+    mpz_mul(exponent8, exponent, eight);
+
     // Calculate bigNumber = 2^exponent - 1.
     mpz_pow_ui(bigNumber, two, atoi(argv[1]));
     mpz_sub(bigNumber, bigNumber, one);
-    gmp_printf("Number  : %Zd\n", bigNumber);
+    gmp_printf("Number     : %Zd\n", bigNumber);
 
-    // Calculate initial starting value.
+    // Calculate initial starting value. square root of the big number
     mpz_sqrt(start, bigNumber);
-    gmp_printf("Sqrt    : %Zd\n", start);
+    // gmp_printf("Sqrt    : %Zd\n", start);
 
     // Decrement start until it is 1 modulo p.
     while (mpz_cmp(mod, one) != 0 || !mpz_odd_p(start))
@@ -70,36 +97,34 @@ int main(int argc, char *argv[])
         mpz_sub(start, start, one);
         mpz_mod(mod, start, exponent);
     }
-    gmp_printf("Start   : %Zd\n", start);
+    // gmp_printf("Start   : %Zd\n", start);
 
-    // Main loop: decrement start by p * 2 and check for factors.
-    while (1)
+    // Calculating two starting points 1 or 7 in mod 8.
+    while (mpz_cmp(checkMod1, zero) == 0 || mpz_cmp(checkMod7, zero) == 0)
     {
         mpz_mod(mod, start, eight);
-        if (mpz_cmp(mod, one) == 0 || mpz_cmp(mod, seven) == 0)
+        if (mpz_cmp(checkMod1, zero) == 0 && mpz_cmp(mod, one) == 0)
         {
-            if (mpz_cmp(start, one) < 0)
-            {
-                printf("Mersenne Prime !\n");
-                exit(0);
-            }
-            switch (mpz_probab_prime_p(start, 50))
-            {
-            case 1:
-            case 2:
-                // gmp_printf("Check :  %Zd\n", start);
-                mpz_mod(mod, bigNumber, start);
-                if (mpz_cmp(mod, zero) == 0)
-                {
-                    gmp_printf("Factor  : %Zd\n", start);
-                    exit(0);
-                }
-                break;
-            case 0:
-            default:
-                break;
-            }
+            mpz_set(checkMod1, start);
+        }
+        if (mpz_cmp(checkMod7, zero) == 0 && mpz_cmp(mod, seven) == 0)
+        {
+            mpz_set(checkMod7, start);
         }
         mpz_sub(start, start, exponent2);
     }
+    // gmp_printf("checkMod1   : %Zd\n", checkMod1);
+    // gmp_printf("checkMod7   : %Zd\n", checkMod7);
+
+    // Main loop to check numbers
+    while (mpz_cmp(checkMod1, zero) > 0 && mpz_cmp(checkMod7, zero) > 0)
+    {
+        check_factor(bigNumber, checkMod1);
+        mpz_sub(checkMod1, checkMod1, exponent8);
+
+        check_factor(bigNumber, checkMod7);
+        mpz_sub(checkMod7, checkMod7, exponent8);
+    }
+    printf("\nMersenne Prime !\n");
+    exit(0);
 }
